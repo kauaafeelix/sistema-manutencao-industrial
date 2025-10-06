364ff6cacb96f50acd17a53ffa81aaa66886000d
Kauã Felix <kfelixcosta11@gmail.com>
2025-10-06 18:35:57 -0300

    first commit

---
+++ src/main/java/org/example/connection/Connection.java
@@ -0,0 +1,5 @@
+package org.example.connection;
+
+public class Connection {
+
+}
---
+++ src/main/java/org/example/connection/TestConnection.java
@@ -0,0 +1,7 @@
+package org.example.connection;
+
+public class TestConnection {
+    public static void main(String[] args) {
+
+    }
+}
