795642bd745a0f5031a0bc2564797ddb169ab241
Kauã Felix <kfelixcosta11@gmail.com>
2025-10-06 18:47:58 -0300

    feat: implementa tratamento de exceções na conexão com o banco de dados

--- src/main/java/org/example/connection/TestConnection.java
+++ src/main/java/org/example/connection/TestConnection.java
@@ -2,6 +2,11 @@ package org.example.connection;
 
 public class TestConnection {
     public static void main(String[] args) {
-
+        try {
+            Conexao.conectar();
+            System.out.println("Conexão bem-sucedida!");
+        } catch (Exception e) {
+            System.out.println("Falha na conexão: " + e.getMessage());
+        }
     }
 }
