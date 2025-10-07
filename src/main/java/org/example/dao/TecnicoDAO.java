ade52b7ba6bca7ab1a41895a33f3e06b86fd27c2
Kauã Felix <kfelixcosta11@gmail.com>
2025-10-06 21:52:21 -0300

    feat: adiciona método cadastrarTecnico na classe TecnicoDAO

--- src/main/java/org/example/dao/TecnicoDAO.java
+++ src/main/java/org/example/dao/TecnicoDAO.java
@@ -1,4 +1,31 @@
 package org.example.dao;
 
+import org.example.connection.Conexao;
+import org.example.model.Tecnico;
+
+import java.io.PipedReader;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
 public class TecnicoDAO {
+
+    public void cadastrarTecnico(Tecnico tecnico) throws SQLException {
+
+        String sql = """
+                INSERT INTO Tecnico (
+                nome,
+                especialidade )
+                VALUES (?, ?);
+                """;
+
+        try (Connection conn = Conexao.conectar();
+             PreparedStatement ps = conn.prepareStatement(sql)){
+
+            ps.setString(1, tecnico.getNome());
+            ps.setString(2, tecnico.getEspecialidade());
+            ps.executeUpdate();
+        }
+
+    }
 }
