2360acc9231ca9461e8a14a742613a3ba2576c16
Kauã Felix <kfelixcosta11@gmail.com>
2025-10-06 21:52:17 -0300

    feat: adiciona método cadastrarMaquina na classe MaquinaDAO

--- src/main/java/org/example/dao/MaquinaDAO.java
+++ src/main/java/org/example/dao/MaquinaDAO.java
@@ -1,4 +1,29 @@
 package org.example.dao;
 
+import org.example.connection.Conexao;
+import org.example.model.Maquina;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
 public class MaquinaDAO {
+
+    public void cadastrarMaquina(Maquina maquina) throws SQLException {
+
+        String sql = """
+                INSERT INTO Maquina (
+                nome,
+                setor,
+                status )
+                VALUES (?, ?, 'OPERACIONAL');
+                """;
+
+        try (Connection conn = Conexao.conectar();
+             PreparedStatement ps = conn.prepareStatement(sql)){
+            ps.setString(1, maquina.getNome());
+            ps.setString(2, maquina.getSetor());
+            ps.executeUpdate();
+        }
+    }
 }
