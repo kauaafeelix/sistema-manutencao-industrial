3c74c393c168f71894da93811aece715589e4b78
Kauã Felix <kfelixcosta11@gmail.com>
2025-10-07 21:51:21 -0300

    feat: adiciona método para cadastrar peças na classe PecaDAO

--- src/main/java/org/example/dao/PecaDAO.java
+++ src/main/java/org/example/dao/PecaDAO.java
@@ -1,4 +1,23 @@
 package org.example.dao;
 
+import org.example.connection.Conexao;
+import org.example.model.Peca;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
 public class PecaDAO {
+
+    public void cadastrarPeca(Peca peca) throws SQLException {
+
+        String sql = "INSERT INTO Peca (nome, estoque) VALUES (?, ?)";
+
+        try (Connection conn = Conexao.conectar();
+             PreparedStatement ps = conn.prepareStatement(sql)){
+
+            ps.setString(1, peca.getNome());
+            ps.setDouble(2, peca.getEstoque());
+        }
+    }
 }
