1c9705a094a37ed4446183ea93a3dfbb57083e9f
Kauã Felix <kfelixcosta11@gmail.com>
2025-10-07 21:51:41 -0300

    feat: adiciona método para cadastrar técnicos na classe ViewTecnico

--- src/main/java/org/example/view/ViewTecnico.java
+++ src/main/java/org/example/view/ViewTecnico.java
@@ -1,6 +1,27 @@
 package org.example.view;
 
+import org.example.controller.ControllerTecnico;
+
+import java.sql.SQLException;
+import java.util.Scanner;
+
 public class ViewTecnico {
+    static Scanner scNum = new Scanner (System.in);
+    static Scanner scStr = new Scanner(System.in);
+    public static void cadastrarTecnico(){
+        System.out.println("Digite o nome do Técnico: ");
+        String nome = scStr.nextLine();
 
+        System.out.println("Digite a especialidade: ");
+        String especialidade = scStr.nextLine();
 
+        var controllerTecnico = new ControllerTecnico();
+
+        try{
+            controllerTecnico.cadastrarTecnico(nome, especialidade);
+            System.out.println("Técnico cadastrado com sucesso!");
+        } catch (SQLException e) {
+            System.out.println("Erro ao cadastrar técnico "+ e.getMessage());
+        }
+    }
 }
