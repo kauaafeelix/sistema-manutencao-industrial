5705ce8f3d6743ec0f1ccb35746eb8a3ea23a9b6
Kauã Felix <kfelixcosta11@gmail.com>
2025-10-09 18:59:41 -0300

    feat: altera tratamento de exceção para SQLException na classe TestConnection

--- src/main/java/org/example/connection/TestConnection.java
+++ src/main/java/org/example/connection/TestConnection.java
@@ -1,11 +1,13 @@
 package org.example.connection;
 
+import java.sql.SQLException;
+
 public class TestConnection {
     public static void main(String[] args) {
         try {
             Conexao.conectar();
             System.out.println("Conexão bem-sucedida!");
-        } catch (Exception e) {
+        } catch (SQLException e) {
             System.out.println("Falha na conexão: " + e.getMessage());
         }
     }
