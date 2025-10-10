51b5852adb6d89d6626c94975fc09db16c304de9
Kauã Felix <kfelixcosta11@gmail.com>
2025-10-09 21:43:59 -0300

    feat: implementa controlador para gerenciar peças e integra com a classe PecaService

--- src/main/java/org/example/controller/ControllerPeca.java
+++ src/main/java/org/example/controller/ControllerPeca.java
@@ -2,9 +2,22 @@ package org.example.controller;
 
 import org.example.dao.PecaDAO;
 import org.example.model.Peca;
+import org.example.service.PecaService;
+import org.example.view.ViewPeca;
 
 import java.sql.SQLException;
 
 public class ControllerPeca {
 
+    ViewPeca viewPeca = new ViewPeca();
+    PecaService pecaService = new PecaService();
+
+    public void pecaController(){
+        switch (viewPeca.menuPeca()){
+
+            case 1-> {
+                pecaService.cadastrarPeca();
+            }
+        }
+    }
 }
