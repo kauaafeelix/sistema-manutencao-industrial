4d21711510c2e903b0ee409971778a77be2ad1bd
Kauã Felix <kfelixcosta11@gmail.com>
2025-10-09 21:43:46 -0300

    feat: implementa controlador para gerenciar máquinas e integra com a classe MaquinaService

--- src/main/java/org/example/controller/ControllerMaquina.java
+++ src/main/java/org/example/controller/ControllerMaquina.java
@@ -2,10 +2,23 @@ package org.example.controller;
 
 import org.example.dao.MaquinaDAO;
 import org.example.model.Maquina;
+import org.example.service.MaquinaService;
+import org.example.view.ViewMaquina;
 
 import java.sql.SQLException;
 
 public class ControllerMaquina {
 
+    ViewMaquina viewMaquina = new ViewMaquina();
+    MaquinaService maquinaService = new MaquinaService();
+
+    public void maquinaController(){
+        switch (viewMaquina.menuMaquina()){
+
+            case 1-> {
+                maquinaService.cadastrarMaquina();
+            }
+        }
+    }
 
 }
