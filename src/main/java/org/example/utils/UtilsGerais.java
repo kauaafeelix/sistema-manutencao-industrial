a88ae788305ac69d1dfbb4ff4c218d1b0001089b
Kauã Felix <kfelixcosta11@gmail.com>
2025-10-09 21:45:41 -0300

    feat: cria a classe UtilsGerais para exibir mensagens de cadastro

---
+++ src/main/java/org/example/utils/UtilsGerais.java
@@ -0,0 +1,10 @@
+package org.example.utils;
+
+public class UtilsGerais {
+    public void mostarMensagemCadastro(){
+        System.out.println("[OK] Cadastro realizado com sucesso!");
+    }
+    public void mostrarMensagemErro(){
+        System.out.println("[ERRO] Erro ao cadastrar.");
+    }
+}
