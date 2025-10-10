79ab0bec9d424011bfa8d32aaef4386473ec400a
Kauã Felix <kfelixcosta11@gmail.com>
2025-10-09 21:44:04 -0300

    feat: implementa controlador para gerenciar técnicos e integra com a classe TecnicoService

--- src/main/java/org/example/controller/ControllerTecnico.java
+++ src/main/java/org/example/controller/ControllerTecnico.java
@@ -2,9 +2,21 @@ package org.example.controller;
 
 import org.example.dao.TecnicoDAO;
 import org.example.model.Tecnico;
+import org.example.service.TecnicoService;
+import org.example.view.ViewTecnico;
 
 import java.sql.SQLException;
 
 public class ControllerTecnico {
 
+    ViewTecnico viewTecnico = new ViewTecnico();
+    TecnicoService tecnicoService = new TecnicoService();
+
+    public void tecnicoController(){
+        switch (viewTecnico.menuTecnico()){
+            case 1-> {
+                tecnicoService.cadastrarTecnico();
+            }
+        }
+    }
 }
