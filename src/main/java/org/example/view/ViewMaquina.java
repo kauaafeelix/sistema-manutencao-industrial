274ca93ede2e2ea06274504dacd66f1841622b20
Kauã Felix <kfelixcosta11@gmail.com>
2025-10-09 21:45:46 -0300

    feat: adiciona menu de opções para gerenciamento de máquinas

--- src/main/java/org/example/view/ViewMaquina.java
+++ src/main/java/org/example/view/ViewMaquina.java
@@ -12,25 +12,31 @@ public class ViewMaquina {
     Scanner scNum = new Scanner(System.in);
     Scanner scStr = new Scanner(System.in);
 
+    public int menuMaquina() {
+        System.out.println("====== MENU MÁQUINA ======");
+        System.out.println("1 - Cadastrar Máquina");
+        System.out.println("2 - Listar Máquinas");
+        System.out.println("3 - Atualizar Máquina");
+        System.out.println("4 - Deletar Máquina");
+        System.out.println("0 - Voltar ao Menu Principal");
+        System.out.print("Escolha uma opção: ");
+        int opcao = scNum.nextInt();
+        return opcao;
+    }
+
+
     public Maquina cadastroMaquina() {
-        System.out.println("====== CADASTRO DE MÁQUINA ======");
+            System.out.println("====== CADASTRO DE MÁQUINA ======");
 
-        System.out.println("Digite o nome da máquina: ");
-        String nome = scStr.nextLine();
+            System.out.println("Digite o nome da máquina: ");
+            String nome = scStr.nextLine();
 
-        System.out.println("Digite o setor da máquina: ");
-        String setor = scStr.nextLine();
+            System.out.println("Digite o setor da máquina: ");
+            String setor = scStr.nextLine();
 
         var novaMaquina = new Maquina(nome, setor);
 
         return novaMaquina;
     }
 
-    public void mostarMensagemCadastro(){
-        System.out.println("[OK] Máquina cadastrada com sucesso!");
-    }
-
-    public void mostrarMensagemErro(){
-        System.out.println("[ERRO] Erro ao cadastrar máquina.");
-    }
 }
