e8c64a4a635e50d12eb462b7c5dffe92360795d4
Kauã Felix <kfelixcosta11@gmail.com>
2025-10-14 19:58:08 -0300

    feat: adiciona classe PecaUtils para exibir informações de peças

---
+++ src/main/java/org/example/utils/PecaUtils.java
@@ -0,0 +1,23 @@
+package org.example.utils;
+
+import org.example.model.Peca;
+
+import java.util.List;
+
+public class PecaUtils {
+
+    public void exibirPeca(List<Peca>pecas){
+        if(pecas !=null && !pecas.isEmpty()){
+            System.out.println("----- Lista de Peças -----");
+            for(Peca peca : pecas){
+                System.out.println("ID: " + peca.getId());
+                System.out.println("Nome: " + peca.getNome());
+                System.out.println("Estoque: " + peca.getEstoque());
+                System.out.println("--------------------------");
+            }
+        } else {
+            System.out.println("Nenhuma peça encontrada.");
+        }
+
+    }
+}
