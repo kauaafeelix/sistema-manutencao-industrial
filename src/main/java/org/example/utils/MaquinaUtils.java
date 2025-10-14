3375a921756bdd6a3fac6366847c85ebe6b8a2d4
Kauã Felix <kfelixcosta11@gmail.com>
2025-10-14 18:37:42 -0300

    feat: adiciona metodo com for-each para percorrer um ArrayList

---
+++ src/main/java/org/example/utils/MaquinaUtils.java
@@ -0,0 +1,24 @@
+package org.example.utils;
+
+import org.example.model.Maquina;
+
+import java.util.List;
+import java.util.Scanner;
+
+public class MaquinaUtils {
+
+    Scanner scNum = new Scanner(System.in);
+    Scanner scStr = new Scanner(System.in);
+
+    public void exibirMaquinas(List<Maquina>maquinas){
+        if(maquinas!= null && !maquinas.isEmpty()){
+            for(Maquina m : maquinas){
+                System.out.println("------LISTA DE MÁQUINAS CADASTRADAS------");
+                System.out.println("\nID: " + m.getId() + "\n | Nome: " + m.getNome() + "\n | Setor: " + m.getSetor() + "\n Status: "+ m.getStatus());
+
+            }
+        }else{
+            System.out.println("[AVISO] Nenhuma máquina cadastrada.");
+        }
+    }
+}
