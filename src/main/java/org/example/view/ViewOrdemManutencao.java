9cdda6a9e53114c47c24132fbb5a8cb02b1dfaa1
Kauã Felix <kfelixcosta11@gmail.com>
2025-10-14 21:47:35 -0300

    refactor: atualiza métodos para retornarem um id da maquina e tecnico

--- src/main/java/org/example/view/ViewOrdemManutencao.java
+++ src/main/java/org/example/view/ViewOrdemManutencao.java
@@ -18,7 +18,7 @@ public class ViewOrdemManutencao {
         System.out.println("3 - Excluir Ordem de Manutenção");
         System.out.println("4 - Listar Ordens de Manutenção");
         System.out.println("5 - Buscar Ordem de Manutenção por ID");
-        System.out.println("6 - Voltar ao Menu Principal");
+        System.out.println("0 - Voltar ao Menu Principal");
         System.out.print("Escolha uma opção: ");
 
         int opcao = scNum.nextInt();
@@ -29,19 +29,15 @@ public class ViewOrdemManutencao {
         System.out.println("----- Criar Ordem de Manutenção -----");
 
     }
-    public OrdemManutencao criarOrdemManutencaoIdMaquina(){
+    public int criarOrdemManutencaoIdMaquina(){
         System.out.println("Digite o ID da Máquina: ");
         int idMaquina = scNum.nextInt();
-        Maquina maquina = new Maquina();
-        maquina.setId(idMaquina);
-        return new OrdemManutencao(maquina, null);
+        return idMaquina;
     }
-    public OrdemManutencao criarOrdemManutencaoIdTecnico(){
+    public int criarOrdemManutencaoIdTecnico(){
         System.out.println("Digite o ID do Técnico: ");
         int idTecnico = scNum.nextInt();
-        Tecnico tecnico = new Tecnico();
-        tecnico.setId(idTecnico);
-        return new OrdemManutencao(null, tecnico);
+        return idTecnico;
     }
 
 }
