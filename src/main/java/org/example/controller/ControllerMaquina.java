cd3ecdb5461e0c61b914d40794f59f91f0af3080
Kauã Felix <kfelixcosta11@gmail.com>
2025-10-14 21:48:36 -0300

    feat: adiciona loop para manter o menu da máquina ativo

--- src/main/java/org/example/controller/ControllerMaquina.java
+++ src/main/java/org/example/controller/ControllerMaquina.java
@@ -3,6 +3,7 @@ package org.example.controller;
 import org.example.dao.MaquinaDAO;
 import org.example.model.Maquina;
 import org.example.service.MaquinaService;
+import org.example.view.ViewGeral;
 import org.example.view.ViewMaquina;
 
 import java.sql.SQLException;
@@ -12,13 +13,19 @@ public class ControllerMaquina {
     ViewMaquina viewMaquina = new ViewMaquina();
     MaquinaService maquinaService = new MaquinaService();
 
-    public void maquinaController(){
-        switch (viewMaquina.menuMaquina()){
+    public void maquinaController() {
 
-            case 1-> {
-                maquinaService.cadastrarMaquina();
+        do {
+            switch (viewMaquina.menuMaquina()) {
+
+                case 1 -> {
+                    maquinaService.cadastrarMaquina();
+                }
+                case 0 ->{
+                    ViewGeral.menuGeral();
+                }
             }
-        }
+        }while (viewMaquina.menuMaquina() != 0);
     }
 
 }
