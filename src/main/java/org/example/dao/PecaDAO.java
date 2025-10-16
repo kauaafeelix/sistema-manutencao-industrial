bb296b00dc41ce6147729ec8b18799dd8d32f792
Kauã Felix <kfelixcosta11@gmail.com>
2025-10-16 19:34:41 -0300

    feat: implementa lógica para cadastrar peça e associá-la à OrdemPeca

--- src/main/java/org/example/dao/PecaDAO.java
+++ src/main/java/org/example/dao/PecaDAO.java
@@ -1,23 +1,54 @@
 package org.example.dao;
 
 import org.example.connection.Conexao;
+import org.example.model.OrdemManutencao;
+import org.example.model.OrdemPeca;
 import org.example.model.Peca;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 
 public class PecaDAO {
 
     public void cadastrarPeca(Peca peca) throws SQLException {
 
-        String sql = "INSERT INTO Peca (nome, estoque) VALUES (?, ?)";
+        String sqlPeca = "INSERT INTO Peca (nome, estoque) VALUES (?, ?)";
 
-        try (Connection conn = Conexao.conectar();
-             PreparedStatement ps = conn.prepareStatement(sql)){
+        String sqlGetId = """
+                SELECT id FROM Peca ORDER BY desc LIMIT 1
+                """;
 
-            ps.setString(1, peca.getNome());
-            ps.setDouble(2, peca.getEstoque());
+        String sqlOrdemPeca = """
+                INSERT IGNORE INTO OrdemPeca
+                (idOrdem, idPeca, quantidade)
+                VALUES (?, ?, ?)
+                """;
+        try (Connection conn = Conexao.conectar()) {
+
+            try (PreparedStatement psPeca = conn.prepareStatement(sqlPeca)) {
+                psPeca.setString(1, peca.getNome());
+                psPeca.setDouble(2, peca.getEstoque());
+                psPeca.executeUpdate();
+            }
+
+            try (PreparedStatement psGetId = conn.prepareStatement(sqlGetId);
+                 ResultSet rs = psGetId.executeQuery()) {
+                if (rs.next()) {
+                    rs.getInt("id");
+                    return;
+                }
+            }
+
+            try (PreparedStatement psOrdemPeca = conn.prepareStatement(sqlOrdemPeca)) {
+                var op = new OrdemPeca();
+                var ordem = new OrdemManutencao();
+                psOrdemPeca.setInt(1, ordem.getId());
+                psOrdemPeca.setInt(2, peca.getId());
+                psOrdemPeca.setDouble(3, op.getQuantidade());
+
+            }
         }
     }
 }
