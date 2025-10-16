fbfb0306ba40ad4c8c8b4deac6964925c0f2fa31
Kauã Felix <kfelixcosta11@gmail.com>
2025-10-16 18:57:57 -0300

    feat: adiciona novas querys para inserir id autoincrementado da tabela OrdemManutencao na tabela associativa

--- src/main/java/org/example/dao/OrdemManutencaoDAO.java
+++ src/main/java/org/example/dao/OrdemManutencaoDAO.java
@@ -2,11 +2,9 @@ package org.example.dao;
 
 import org.example.connection.Conexao;
 import org.example.model.OrdemManutencao;
+import org.example.model.Peca;
 
-import java.sql.Connection;
-import java.sql.Date;
-import java.sql.PreparedStatement;
-import java.sql.SQLException;
+import java.sql.*;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -23,11 +21,22 @@ public class OrdemManutencaoDAO {
               VALUES (?, ?, NOW(), 'PENDENTE')
               """;
 
+      String sqlGetId = "SELECT id FROM OrdemManutencao ORDER BY id DESC LIMIT 1";
+
+      String sqlOrdemPeca = """
+              INSERT INTO OrdemPeca (
+              idOrdemManutencao,
+              idPeca,
+              quantidade)
+              VALUES (?, ?, ?)
+              """;
+
       String sqlMaquina = """
               UPDATE Maquina
               SET status = 'EM_MANUTENCAO'
               WHERE id = ?
               """;
+
 
       try (Connection conn = Conexao.conectar()) {
 
@@ -38,9 +47,24 @@ public class OrdemManutencaoDAO {
               psOrdem.executeUpdate();
           }
 
+          try (PreparedStatement psGetId = conn.prepareStatement(sqlGetId);
+               ResultSet rs = psGetId.executeQuery()) {
+              if (rs.next()) {
+                  rs.getInt("id");
+                  return;
+              }
+          }
+
           try(PreparedStatement psMaquina = conn.prepareStatement(sqlMaquina)){
               psMaquina.setInt(1, ordem.getIdMaquina().getId());
               psMaquina.executeUpdate();
+          }
+
+          try(PreparedStatement psOrdemPeca = conn.prepareStatement(sqlOrdemPeca)){
+              var peca = new Peca();
+              psOrdemPeca.setInt(1, ordem.getId());
+              psOrdemPeca.setInt(2, peca.getId());
+
           }
       }
   }
