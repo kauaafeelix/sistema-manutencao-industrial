12024d11100857ebb3ff217c398fc308c296edf6
Kauã Felix <kfelixcosta11@gmail.com>
2025-10-16 21:50:35 -0300

    refactor: substitui chamada estática de menuGeral por instância de ViewGeral para melhor encapsulamento

--- src/main/java/org/example/controller/ControllerTecnico.java
+++ src/main/java/org/example/controller/ControllerTecnico.java
@@ -3,6 +3,7 @@ package org.example.controller;
 import org.example.dao.TecnicoDAO;
 import org.example.model.Tecnico;
 import org.example.service.TecnicoService;
+import org.example.view.ViewGeral;
 import org.example.view.ViewTecnico;
 
 import java.sql.SQLException;
@@ -11,14 +12,17 @@ public class ControllerTecnico {
 
     ViewTecnico viewTecnico = new ViewTecnico();
     TecnicoService tecnicoService = new TecnicoService();
+    ViewGeral viewGeral = new ViewGeral();
 
     public void tecnicoController(){
 
         do{
-
         switch (viewTecnico.menuTecnico()){
             case 1-> {
                 tecnicoService.cadastrarTecnico();
+            }
+            case 0->{
+                viewGeral.menuGeral();
             }
         }
         }while(viewTecnico.menuTecnico() != 0);
