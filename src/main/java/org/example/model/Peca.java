b0b4af92744644aa115b3457da095b7823196308
Kauã Felix <kfelixcosta11@gmail.com>
2025-10-16 21:51:21 -0300

    refactor: altera tipo de estoque de Double para double para maior eficiência

--- src/main/java/org/example/model/Peca.java
+++ src/main/java/org/example/model/Peca.java
@@ -3,19 +3,19 @@ package org.example.model;
 public class Peca {
     private int id;
     private String nome;
-    private Double estoque;
+    private double estoque;
 
 
     public Peca(){
     }
 
-    public Peca(int id, String nome, Double estoque) {
+    public Peca(int id, String nome, double estoque) {
         this.id = id;
         this.nome = nome;
         this.estoque = estoque;
     }
 
-    public Peca(String nome, Double estoque) {
+    public Peca(String nome, double estoque) {
         this.nome = nome;
         this.estoque = estoque;
     }
@@ -36,11 +36,11 @@ public class Peca {
         this.nome = nome;
     }
 
-    public Double getEstoque() {
+    public double getEstoque() {
         return estoque;
     }
 
-    public void setEstoque(Double estoque) {
+    public void setEstoque(double estoque) {
         this.estoque = estoque;
     }
 }
