82389db9b142b7993b7e4b69d807b75a0b02cd12
Kauã Felix <kfelixcosta11@gmail.com>
2025-10-20 20:48:30 -0300

    feat: atualiza controlador de ordens de peça para utilizar serviço correto

--- src/main/java/org/example/controller/ControllerOrdemPeca.java
+++ src/main/java/org/example/controller/ControllerOrdemPeca.java
@@ -1,19 +1,21 @@
 package org.example.controller;
 
+import org.example.model.OrdemPeca;
 import org.example.service.OrdemManutencaoService;
+import org.example.service.OrdemPecaService;
 import org.example.view.ViewGeral;
 import org.example.view.ViewOrdemPeca;
 
 public class ControllerOrdemPeca {
     ViewOrdemPeca viewOrdemPeca = new ViewOrdemPeca();
-    OrdemManutencaoService ordemManutencaoService = new OrdemManutencaoService();
+    OrdemPecaService ordemPecaService = new OrdemPecaService();
     ViewGeral viewGeral = new ViewGeral();
 
     public void ordemPecaController() {
         do {
             switch (viewOrdemPeca.menuOrdemPeca()) {
                 case 1 ->{
-                    ordemManutencaoService.cadastrarOrdemManutencao();
+                    ordemPecaService.cadastrarOrdemPeca();
                 }
                 case 0 ->{
                     viewGeral.menuGeral();
