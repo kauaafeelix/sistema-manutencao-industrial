4725e8092dc6520fe727927bb1dd0a9a5524f3cf
Kauã Felix <kfelixcosta11@gmail.com>
2025-10-20 18:47:38 -0300

    feat: implementa método para cadastrar OrdemPeca no banco de dados

--- src/main/java/org/example/dao/OrdemPecaDAO.java
+++ src/main/java/org/example/dao/OrdemPecaDAO.java
@@ -1,4 +1,27 @@
 package org.example.dao;
 
+import org.example.connection.Conexao;
+import org.example.model.OrdemPeca;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
 public class OrdemPecaDAO {
+    public void cadastrarOrdemPeca(OrdemPeca ordemPeca) throws SQLException {
+
+        String sql = """
+                INSERT INTO OrdemPeca (idOrdem, idPeca, quantidade)
+                VALUES (?, ?, ?)
+                """;
+
+        try (Connection conn = Conexao.conectar();
+             PreparedStatement ps = conn.prepareStatement(sql)){
+
+            ps.setInt(1, ordemPeca.getIdOrdem().getId());
+            ps.setInt(2, ordemPeca.getIdPeca().getId());
+            ps.setDouble(3, ordemPeca.getQuantidade());
+            ps.executeUpdate();
+        }
+    }
 }
