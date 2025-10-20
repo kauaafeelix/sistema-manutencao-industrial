18c909123ab8010da908074407279019814da354
Kauã Felix <kfelixcosta11@gmail.com>
2025-10-20 20:09:23 -0300

    feat: implementa controlador para gerenciar ordens de peça com menu interativo

--- src/main/java/org/example/controller/ControllerOrdemPeca.java
+++ src/main/java/org/example/controller/ControllerOrdemPeca.java
@@ -1,4 +1,24 @@
 package org.example.controller;
 
+import org.example.service.OrdemManutencaoService;
+import org.example.view.ViewGeral;
+import org.example.view.ViewOrdemPeca;
+
 public class ControllerOrdemPeca {
+    ViewOrdemPeca viewOrdemPeca = new ViewOrdemPeca();
+    OrdemManutencaoService ordemManutencaoService = new OrdemManutencaoService();
+    ViewGeral viewGeral = new ViewGeral();
+
+    public void ordemPecaController() {
+        do {
+            switch (viewOrdemPeca.menuOrdemPeca()) {
+                case 1 ->{
+                    ordemManutencaoService.cadastrarOrdemManutencao();
+                }
+                case 0 ->{
+                    viewGeral.menuGeral();
+                }
+            }
+        }while (viewOrdemPeca.menuOrdemPeca() != 0);
+    }
 }
