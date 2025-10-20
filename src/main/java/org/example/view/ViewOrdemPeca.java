48eeaa93c67f3df75a20c275c8743503762ae79e
Kauã Felix <kfelixcosta11@gmail.com>
2025-10-20 20:09:19 -0300

    feat: altera métodos de cadastro para retornar valores e adiciona mensagens de erro para validação

--- src/main/java/org/example/view/ViewOrdemPeca.java
+++ src/main/java/org/example/view/ViewOrdemPeca.java
@@ -4,28 +4,44 @@ import java.util.Scanner;
 
 public class ViewOrdemPeca {
 
-    Scanner scNum = new Scanner (System.in);
+    Scanner scNum = new Scanner(System.in);
     Scanner scStr = new Scanner(System.in);
 
-    public void menuOrdemPeca(){
+    public int menuOrdemPeca() {
         System.out.println("\n----- Menu de Peças na Ordem de Manutenção -----");
         System.out.println("1 - Cadastrar Peça na Ordem de Manutenção");
         System.out.println("2 - Voltar ao Menu Principal");
         System.out.print("Escolha uma opção: ");
+        int opcao = scNum.nextInt();
+        return opcao;
     }
 
-    public void cadastroOrdemPecaIdOrdemManutencao(){
+    public int cadastroOrdemPecaIdOrdemManutencao() {
         System.out.print("Digite o ID da Ordem de Manutenção: ");
         int idOrdem = scNum.nextInt();
+
+        return idOrdem;
     }
 
-    public void cadastroOrdemPecaIdPeca(){
+    public int cadastroOrdemPecaIdPeca() {
         System.out.print("Digite o ID da Peça: ");
         int idPeca = scNum.nextInt();
+        return idPeca;
     }
 
-    public void cadastroOrdemPecaQuantidade(){
+    public double cadastroOrdemPecaQuantidade() {
         System.out.print("Digite a quantidade da Peça: ");
         double quantidade = scNum.nextDouble();
+
+        return quantidade;
     }
+
+    public void mensagemMenorEstoque() {
+        System.err.println("[ERRO] A quantidade solicitada é maior que o estoque disponível.");
+    }
+
+    public void naoPodeSerZeroOuNegativo() {
+        System.err.println("[ERRO] A quantidade deve ser maior que zero.");
+    }
+
 }
